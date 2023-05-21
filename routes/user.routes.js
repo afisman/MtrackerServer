2c230e55106765ca446a4b8372e678830a810ae0
afisman <afisman@gmail.com>
2023-05-21 19:23:54 +0200

    updated user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,15 @@
 const router = require("express").Router()
+const { isAuthenticated } = require("../middlewares/jwt.middlewares")
 const User = require('../models/User')
 
 
 
-router.get("/:user_id", (req, res) => {
+router.get("/:user_id", isAuthenticated, (req, res, next) => {
     const { user_id } = req.params
 
     User
         .findById(user_id)
+        .populate('cycles')
         .then(response => res.json(response))
         .catch(err => res.status(500).json(err))
 })
