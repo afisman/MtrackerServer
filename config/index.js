42dad5b75a077bdc471de211c49e59a8e9fd8425
afisman <afisman@gmail.com>
2023-05-27 20:11:39 +0200

    a

--- config/index.js
+++ config/index.js
@@ -6,23 +6,17 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 
-const whitelist = [process.env.ORIGIN]
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error("Not allowed by CORS"))
-        }
-    },
-    credentials: true,
-}
+
 
 module.exports = (app) => {
     app.set("trust proxy", 1);
 
-    app.use(cors(corsOptions))
-
+    app.use(
+        cors({
+            credentials: true,
+            origin: process.env.ORIGIN || "http://localhost:3000",
+        })
+    );
 
     app.use(logger("dev"));
 
