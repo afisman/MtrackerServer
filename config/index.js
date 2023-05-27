0ca496ad50fbc237ca38bf6355a3feccaf2e615a
afisman <afisman@gmail.com>
2023-05-27 20:06:01 +0200

    updated index config for cors

--- config/index.js
+++ config/index.js
@@ -6,15 +6,23 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 
+const whitelist = [process.env.ORIGIN]
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true,
+}
+
 module.exports = (app) => {
     app.set("trust proxy", 1);
 
-    app.use(
-        cors({
-            credentials: true,
-            origin: process.env.ORIGIN || "http://localhost:3000",
-        })
-    );
+    app.use(cors(corsOptions))
+
 
     app.use(logger("dev"));
 
